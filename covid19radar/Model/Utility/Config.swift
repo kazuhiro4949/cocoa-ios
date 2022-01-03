f4d7ac7f2dbab048ba7e10376da154b26cea9110
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2022-01-03 20:16:08 +0900

    change name

--- covid19radar/Model/Utility/Config.swift
+++ covid19radar/Model/Utility/Config.swift
@@ -16,7 +16,7 @@ struct Config: Codable {
 
 extension Config {
     static let dev: Config = {
-        let url = Bundle.main.url(forResource: "Config", withExtension: "plist")!
+        let url = Bundle.main.url(forResource: "DevEnvironment", withExtension: "plist")!
         let decoder = PropertyListDecoder()
         return try! decoder.decode(Config.self, from: Data(contentsOf: url))
     }()
